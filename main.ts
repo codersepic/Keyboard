cbeca2a5bc7b36dad90ea40dab16ac775234b59c
codersepic <167995964+codersepic@users.noreply.github.com>
2024-04-25 12:24:34 -0400

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,7 +1,7 @@
 let Name = game.askForString("What is your Name?")
 Name = game.askForString(Name == game.askForString("What is your Name?"))
 if (true) {
-	
+    game.gameOver(true)
 } else {
-	
+    game.gameOver(false)
 }
