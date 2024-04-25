eb4bdb516210a39277167078f7a0180154e4c139
codersepic <167995964+codersepic@users.noreply.github.com>
2024-04-25 12:21:59 -0400

    Initial files for MakeCode project

---
+++ main.py
@@ -0,0 +1,6 @@
+Name = game.ask_for_string("What is your Name?")
+Name = game.ask_for_string(Name == game.ask_for_string("What is your Name?"))
+if True:
+    pass
+else:
+    pass
---
+++ main.ts
@@ -0,0 +1,7 @@
+let Name = game.askForString("What is your Name?")
+Name = game.askForString(Name == game.askForString("What is your Name?"))
+if (true) {
+	
+} else {
+	
+}
